a96d8142a0aff3fd101b38e112de55d9689f8e50
Luca Eliseo <121867356+LucaEliseo@users.noreply.github.com>
2023-02-06 16:29:45 +0100

    Add files via upload

---
+++ S2L1 - Introduzione Javascript/assets/javascript/naming.js
@@ -0,0 +1,11 @@
+var animalePreferito = 'Criceto';
+console.log(animalePreferito);
+var Animalepreferito = 'Coniglio';
+console.log(Animalepreferito);
+var animale_preferito = 'Giraffa'
+console.log(animale_preferito);
+
+
+var animale ='Pantera';
+console.log(animale);
+document.write(animale + '&nbsp<span>scritto da javascript</span>');
